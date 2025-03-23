2f6d4dbfaa5ed3212599e9783a15df7fa2ac959c
Thalia Cirqueira <thalia.cirqueira@gmail.com>
2025-03-23 15:56:57 -0300

    Atividade
    Atualização de Exercícios

--- 02ResultadoEntrada_Saida.py
+++ 02ResultadoEntrada_Saida.py
@@ -1,13 +1,9 @@
 
-# 1 Solicite ao usuário que insira seu nome e imprima uma mensagem de saudação.
-nome = input("Digite seu nome: ")
-# 2 Solicite a idade do usuário e imprima uma mensagem indicando se é maior de idade.
-idade = int(input("Insira sua idade: "))
 
+# 2 Peça dois números ao usuário, some-os e imprima o resultado.
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segunndo número: "))
 
-print("Olá", nome, "Que sua jornada aqui seja repleta de Luz! Bem vindo(a)!")
+soma = numero1 + numero2
 
-if idade >= 18:
-    print("Você é maior de idade!")
-else:
-    print("Você não é maior de idade")
+print(f"A soma de {numero1} e {numero2} é {soma}.")
--- 04ResultadoEntrada_Saida.py
+++ 04ResultadoEntrada_Saida.py
@@ -1,13 +1,2 @@
 
-# 1 Solicite ao usuário que insira seu nome e imprima uma mensagem de saudação.
-nome = input("Digite seu nome: ")
-# 2 Solicite a idade do usuário e imprima uma mensagem indicando se é maior de idade.
-idade = int(input("Insira sua idade: "))
-
-
-print("Olá", nome, "Que sua jornada aqui seja repleta de Luz! Bem vindo(a)!")
-
-if idade >= 18:
-    print("Você é maior de idade!")
-else:
-    print("Você não é maior de idade")
+# 4 Peça o raio de um círculo e calcule sua área. Em seguida, imprima o resultado.
