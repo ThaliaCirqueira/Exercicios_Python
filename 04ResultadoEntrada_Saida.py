99bfe803f8d6cab85bdc6262039ea7ae36a6aa15
Thalia Cirqueira <thalia.cirqueira@gmail.com>
2025-03-24 15:33:42 -0300

    Atividades
    Atualização de exercícios

--- 04ResultadoEntrada_Saida.py
+++ 04ResultadoEntrada_Saida.py
@@ -1,2 +1,10 @@
 
 # 4 Peça o raio de um círculo e calcule sua área. Em seguida, imprima o resultado.
+
+import math
+
+raio = float(input("Insira o Raio do círculo:"))
+
+area = math.pi * (raio ** 2)
+
+print(f"A área do círculo com {raio} é {area:.2f}.")
